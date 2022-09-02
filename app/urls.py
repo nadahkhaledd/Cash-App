a708c72618a62faf6219ca4cc05590921bc5a96d
nadahkhaledd <nadahkhaledd@gmail.com>
2022-09-02 22:41:31 +0200

    added redirect to login and logout

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
     path("", include("django.contrib.auth.urls")),
     path(r'<str:username>', views.profile, name='profile'),
-    path('transfer/', views.transfer, name='transfer')
+    path('transfer/', views.transfer, name='transfer'),
+    path('', TemplateView.as_view(template_name='registration/home.html'), name='home'),
 ]
