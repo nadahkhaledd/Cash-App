75a40aa08682e39e1bf59df734d92a479d2dd6ba
nadahkhaledd <nadahkhaledd@gmail.com>
2022-09-02 22:09:25 +0200

    added view for auth

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path("", include("django.contrib.auth.urls")),
     path(r'<str:username>', views.profile, name='profile'),
     path('transfer/', views.transfer, name='transfer')
 ]
