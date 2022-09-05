972ae6516d4ef9c22e58155a7cc592671e9bc112
nadahkhaledd <nadahkhaledd@gmail.com>
2022-09-05 15:17:39 +0200

    updated last query

--- app/queries/number_of_each_transaction_type_for_day.sql
+++ app/queries/number_of_each_transaction_type_for_day.sql
@@ -1,5 +1,6 @@
 -- get number of each transaction made for a specific date
-SELECT t.type, COUNT(t.type) 'number of transactions', DATE(t.date) 'date'
- FROM cash.transaction t
- WHERE DATE(t.date) = CURDATE()
- GROUP BY t.type;
+
+ SELECT type, COUNT(type) 'number of transactions', DATE(date) 'date'
+ FROM cash.transaction
+ WHERE DATE(date) = CURDATE()
+ GROUP BY type;
