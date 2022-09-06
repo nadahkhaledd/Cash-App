f9842692906170edc2fea8f395c489d8d6451990
nadahkhaledd <nadahkhaledd@gmail.com>
2022-09-06 17:49:15 +0200

    changed to test with sqlite

--- cash/settings.py
+++ cash/settings.py
@@ -97,9 +97,12 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': '5432',
-        'TEST':{'ENGINE': 'django.db.backends.sqlite3','NAME': 'db.sqlite3',}
     }
 }
+
+import sys
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # DATABASES = {
 #     'default': {
