1ab471f5b971cb965db84553d24593e521383fb3
nadahkhaledd <nadahkhaledd@gmail.com>
2022-09-06 17:07:46 +0200

    updated docker

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
+RUN apt-get update
+RUN apt-get install -y netcat
 COPY . /code/
 
---
+++ runserver.sh
@@ -0,0 +1,9 @@
+echo "Waiting for SQL..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.1
+done
+echo "SQL started"
+python http://manage.py makemigrations
+python http://manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='nadah').exists() or User.objects.create_superuser('nadah', 'nadahkhaledd@gmail.com, 'nadah1234')" | python http://manage.py shell
+python http://manage.py runserver 0.0.0.0:8000
